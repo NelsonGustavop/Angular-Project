6fe751aa06620d8af880ed76349bb08f43f064f3
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-20 16:56:11 -0300

    Repository/metodo get

--- src/main/java/com/Slayer/Angular/domain/Tecnico.java
+++ src/main/java/com/Slayer/Angular/domain/Tecnico.java
@@ -8,11 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.Slayer.Angular.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Tecnico extends Pessoa implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
@@ -24,7 +27,7 @@ public class Tecnico extends Pessoa implements Serializable {
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfis(Perfil.TECNICO);
-		
+
 	}
 
 	public List<Chamado> getChamados() {
---
+++ src/main/java/com/Slayer/Angular/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.Slayer.Angular.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Slayer.Angular.domain.Tecnico;
+import com.Slayer.Angular.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+		@Autowired
+		private TecnicoRepository repository;
+		
+		public Tecnico findById(Integer id) {
+			Optional<Tecnico> obj = repository.findById(id);
+			return obj.orElse(null);
+		}
+}
