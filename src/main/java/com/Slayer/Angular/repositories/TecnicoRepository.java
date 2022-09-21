0654d089e5bbdd2890fd279d6ab22d08a6b63a70
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-21 14:46:24 -0300

    Create findAll

--- src/main/java/com/Slayer/Angular/repositories/TecnicoRepository.java
+++ src/main/java/com/Slayer/Angular/repositories/TecnicoRepository.java
@@ -1,7 +1,5 @@
 package com.Slayer.Angular.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Slayer.Angular.domain.Tecnico;
--- src/main/java/com/Slayer/Angular/services/TecnicoService.java
+++ src/main/java/com/Slayer/Angular/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.Slayer.Angular.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,10 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found: "+id));
+	}
+
+	public List<Tecnico> findAll() {
+		
+		return repository.findAll();
 	}
 }
