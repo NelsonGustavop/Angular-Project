3d475761b794aed44b9f5b0c1b76b51ff3d8c33a
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-21 10:54:56 -0300

    Tratamento de Exeção personalizada

--- src/main/java/com/Slayer/Angular/services/TecnicoService.java
+++ src/main/java/com/Slayer/Angular/services/TecnicoService.java
@@ -7,15 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.Slayer.Angular.domain.Tecnico;
 import com.Slayer.Angular.repositories.TecnicoRepository;
+import com.Slayer.Angular.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
 
-		@Autowired
-		private TecnicoRepository repository;
-		
-		public Tecnico findById(Integer id) {
-			Optional<Tecnico> obj = repository.findById(id);
-			return obj.orElse(null);
-		}
+	@Autowired
+	private TecnicoRepository repository;
+
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found: "+id));
+	}
 }
