13cf7454d2a9713d17d60b4f09624e832bc5973d
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-21 19:01:55 -0300

    new3 Exception created

--- src/main/java/com/Slayer/Angular/services/TecnicoService.java
+++ src/main/java/com/Slayer/Angular/services/TecnicoService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.Slayer.Angular.domain.Pessoa;
 import com.Slayer.Angular.domain.Tecnico;
 import com.Slayer.Angular.domain.dtos.TecnicoDTO;
+import com.Slayer.Angular.repositories.PessoaRepository;
 import com.Slayer.Angular.repositories.TecnicoRepository;
 import com.Slayer.Angular.services.exceptions.ObjectNotFoundException;
 
@@ -16,21 +19,35 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found: "+id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("object not found: " + id));
 	}
 
 	public List<Tecnico> findAll() {
-		
+
 		return repository.findAll();
 	}
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
-		
+
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("Email já cadastrado no sistema");
+		}
 	}
 }
---
+++ src/main/java/com/Slayer/Angular/services/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,31 @@
+package com.Slayer.Angular.services.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.Slayer.Angular.resource.exception.StandardError;
+
+public class DataIntegrityViolationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityViolationException(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public DataIntegrityViolationException(String message) {
+		super(message);
+
+	}
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	  public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+		  StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				  "violação de dados", ex.getMessage(), request.getRequestURI());
+		  return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+}
