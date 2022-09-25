4bb73721ae2a21375613811b71b84a47b4de8e5d
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-25 13:50:42 -0300

    Chamado Create

--- src/main/java/com/Slayer/Angular/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/Slayer/Angular/domain/dtos/ChamadoDTO.java
@@ -3,9 +3,7 @@ package com.Slayer.Angular.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import com.Slayer.Angular.domain.Chamado;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,16 +11,23 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 public class ChamadoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	
 	private Integer id;
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo prioridade é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo status é requerido")
 	private Integer status;
+	@NotNull(message = "O campo titulo é requerido")
 	private String titulo;
+	@NotNull(message = "O campo observacoes é requerido")
 	private String observacoes;
+	@NotNull(message = "O campo tecnico é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo cliente é requerido")
 	private Integer cliente;
 	private String NomeTecnico;
 	private String NomeCliente;
--- src/main/java/com/Slayer/Angular/services/ChamadoService.java
+++ src/main/java/com/Slayer/Angular/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.Slayer.Angular.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Slayer.Angular.domain.Chamado;
+import com.Slayer.Angular.domain.Cliente;
+import com.Slayer.Angular.domain.Tecnico;
+import com.Slayer.Angular.domain.dtos.ChamadoDTO;
+import com.Slayer.Angular.domain.enums.Prioridade;
+import com.Slayer.Angular.domain.enums.Status;
 import com.Slayer.Angular.repositories.ChamadoRepository;
 import com.Slayer.Angular.services.exceptions.ObjectNotFoundException;
 
@@ -14,7 +21,10 @@ import com.Slayer.Angular.services.exceptions.ObjectNotFoundException;
 public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
-	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"+id)) ;
@@ -24,4 +34,28 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		
+		return repository.save(newChamado(objDTO));
+	}
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(chamado.getId() !=null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
+	
+	
+	
 }
