5df0a28cc1e93f6df3d03d4b2a882ccfa8b2c335
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-09-25 12:39:13 -0300

    findAll chamado

--- src/main/java/com/Slayer/Angular/services/ChamadoService.java
+++ src/main/java/com/Slayer/Angular/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.Slayer.Angular.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class ChamadoService {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"+id)) ;
 		
+	}
+	
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 }
